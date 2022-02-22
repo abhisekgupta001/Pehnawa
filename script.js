651d5e62a1e1cac8e6222ac4584abe71a9fb4dfd
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-02-22 23:06:41 +0530

    style: added favicon, made sidebar toggleable

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const verticalNav = document.querySelector(".vertical-nav");
 const verticalClose = document.querySelector(".close-vertical-btn");
+const filterBtn = document.querySelector(".filter-icon");
+const filterBtnWrapper = document.querySelector(".filter-icon-wrapper");
+const filterSideBar = document.querySelector(".filter-sidebar");
+
 
 
 hamburgerIcon.addEventListener("click", ()=>{
@@ -9,4 +13,13 @@ hamburgerIcon.addEventListener("click", ()=>{
 
 verticalClose.addEventListener("click", ()=>{
     verticalNav.classList.remove("show-vertical-nav");
+})
+
+filterBtn.addEventListener("click", ()=>{
+    filterSideBar.classList.toggle("show-sidebar");
+    filterBtnWrapper.classList.toggle("move-filter-icon");
+    setTimeout(()=>{
+    filterBtnWrapper.classList.remove("move-filter-icon");
+    filterSideBar.classList.remove("show-sidebar");
+    },6000)
 })
